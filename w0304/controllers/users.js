268bce67214a6b852c607f12dad2660cfc88f976
carly509 <tesnorcarly@gmail.com>
2025-06-01 14:59:06 -0400

    Added more method to users controller

--- w0304/controllers/users.js
+++ w0304/controllers/users.js
@@ -39,38 +39,87 @@ const createUser = async (req, res) => {
   }
 };
 
-
 const getAll = async (req, res) => {
-    //#swagger.tags = ['Users']
-    try {
-        const result = await mongodb.getDb().collection('users').find().toArray();
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(result);
-    } catch (err) {
-        res.status(500).json({ message: 'Error retrieving users', error: err });
-    }
+  //#swagger.tags = ['Users']
+  try {
+    const result = await mongodb.getDb().collection('users').find().toArray();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ message: 'Error retrieving users', error: err });
+  }
 };
 
 const getUser = async (req, res) => {
-    //#swagger.tags = ['Users']
-    try {
-        const userId = new ObjectId(req.params.id);
-        const result = await mongodb.getDb().collection('users').findOne({ _id: userId });
+  //#swagger.tags = ['Users']
+  try {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().collection('users').findOne({ _id: userId });
 
-        if (!result) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(result);
-    } catch (err) {
-        console.error(err); // Log the error for debugging
-        res.status(500).json({ message: 'Error retrieving user', error: err.message });
+    if (!result) {
+      return res.status(404).json({ message: 'User not found' });
     }
+
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Error retrieving user', error: err.message });
+  }
+};
+
+const updateUser = async (req, res) => {
+  //#swagger.tags = ['Users']
+  try {
+    const userId = new ObjectId(req.params.id);
+    const validationError = validateUser(req.body);
+    if (validationError) {
+      return res.status(400).json({ message: validationError });
+    }
+
+    const updatedUser = {
+      username: req.body.username,
+      email: req.body.email,
+      name: req.body.name
+    };
+
+    const result = await mongodb.getDb().collection('users').updateOne(
+      { _id: userId },
+      { $set: updatedUser }
+    );
+
+    if (result.modifiedCount > 0) {
+      res.status(200).json({ message: 'User updated successfully' });
+    } else {
+      res.status(404).json({ message: 'User not found or no changes made' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Error updating user', error: err.message });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  //#swagger.tags = ['Users']
+  try {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().collection('users').deleteOne({ _id: userId });
+
+    if (result.deletedCount > 0) {
+      res.status(200).json({ message: 'User deleted successfully' });
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Error deleting user', error: err.message });
+  }
 };
 
 module.exports = {
-    createUser,
-    getAll,
-    getUser
+  createUser,
+  getAll,
+  getUser,
+  updateUser,
+  deleteUser
 };
--- w0304/routes/users.js
+++ w0304/routes/users.js
@@ -6,5 +6,7 @@ const usersController = require('../controllers/users');
 router.post('/', usersController.createUser);
 router.get('/', usersController.getAll);
 router.get('/:id', usersController.getUser);
+router.put('/:id', usersController.updateUser);
+router.delete('/:id', usersController.deleteUser);
 
 module.exports = router;
